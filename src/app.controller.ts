96f76f432d52c569ed1ad82d88ecb93f0cc0a577
Nur Aziz <sukabaru09@gmail.com>
2025-01-15 17:35:34 +0700

    fix: authentication

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiBearerAuth, ApiSecurity } from '@nestjs/swagger';
-import { ApiKey } from './modules/auth/auth.guard';
+import { Public } from './modules/auth/auth.guard';
 
 @Controller()
 export class AppController {
   constructor() {}
 
   @Get()
-  @ApiBearerAuth()
-  @ApiKey()
-  @ApiSecurity('apikey')
+  @Public()
   default(): string {
     return new Date().toLocaleString();
   }
--- src/modules/sholat/sholat.controller.ts
+++ src/modules/sholat/sholat.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { Public } from '../auth/auth.guard';
 import { SholatService } from './sholat.service';
 
 @ApiTags('Jadwal Sholat')
@@ -8,11 +9,13 @@ export class SholatController {
   constructor(private readonly sholatService: SholatService) {}
 
   @Get()
+  @Public()
   async getJadwalSholat(@Query('id_kota') idKota: string) {
     return await this.sholatService.jadwalSholat(idKota);
   }
 
   @Get('list-kota')
+  @Public()
   async getListKota() {
     return await this.sholatService.listKota();
   }
